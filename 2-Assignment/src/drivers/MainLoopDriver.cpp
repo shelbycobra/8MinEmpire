005d9a0f0b29e3c83040f4ef3affb6c5c49c9de7
shelbycobra <shelbywams@gmail.com>
2019-11-02 11:35:27 -0400

    Started Part 3

---
+++ 2-Assignment/src/drivers/MainLoopDriver.cpp
@@ -0,0 +1,23 @@
+#include "../GameMainLoop.h"
+
+
+int main() {
+    GameMainEngine* gameEngine = new GameMainEngine();
+
+    gameEngine->getStartUpPhase()->startGame();
+
+    string cont;
+
+    while(true) {
+
+        gameEngine->playTurn();
+
+        cout << "CONTINUE DEMO?" << endl;
+        cout << "'y' or 'Y' to continue > ";
+        getline(cin, cont);
+        if(cont != "y" && cont != "Y")
+            break;
+    }
+
+    return 0;
+}
