98d6f075f2fa62b1d449a7a03e5455cb0615040c
shelbycobra <s_wams@live.concordia.ca>
2020-01-19 11:51:46 -0500

    Remove assignments 1,2, and 3

--- 2-Assignment/src/drivers/GameStartUpDriver.cpp
+++
@@ -1,8 +0,0 @@
-#include "../GameStartUp.h"
-
-int main() {
-    GameStartUpEngine startPhase;
-    startPhase.startGame();
-
-    return 0;
-}
--- demo/src/drivers/GameStartUpDriver.cpp
+++
@@ -1,8 +0,0 @@
-#include "../GameStartUp.h"
-
-int main() {
-    StartUpGameEngine startPhase;
-    startPhase.startGame();
-
-    return 0;
-}
